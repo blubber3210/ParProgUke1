a223eac26154f391047936960b6dd089c1f30d62
Elida <blubber3210@gmail.com>
2025-11-26 10:28:37 +0100

    Add project files.

---
+++ ParProgUke1/Deltager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ParProgUke1
+{
+    internal class Deltager
+    {
+        public Deltager(string name, int age, string hairColor, string[] hobbies)
+        {
+            Name = name;
+            Age = age;
+            HairColor = hairColor;
+            Hobbies = hobbies;
+        }
+
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string HairColor { get; set; }
+        public string[] Hobbies { get; set; }
+
+        public void PrintText()
+        {
+            Console.WriteLine("Hei, mitt navn er " + Name);
+            Console.WriteLine($"Jeg er {Age} år gammel");
+            for (int i = 0; i < Hobbies.Length; i++)
+            {
+                Console.WriteLine(Hobbies[i]);
+            }
+        }
+    }
+}
---
+++ ParProgUke1/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ParProgUke1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var Elida = new Deltager("Elida", 30, "Blondt",
+                ["Jeg liker å klatre rundt", "Jeg er skikkelig dårlig til å synge"]);
+        }
+    }
+}
