9e19600203fc3a17505fd798a0a6aa2f6ccf08d4
Elida <blubber3210@gmail.com>
2025-11-26 11:16:25 +0100

    add venn

--- ParProgUke1/Program.cs
+++ ParProgUke1/Program.cs
@@ -6,15 +6,26 @@ namespace ParProgUke1
     {
         static void Main(string[] args)
         {
-            var deltagere = new Deltager[]
-            {
-                new Deltager("Elida", 30, "Blondt",
-                    ["Jeg liker å klatre rundt", "Jeg er skikkelig dårlig til å synge"]),
-                new Deltager("Christian", 36, "Havanna", ["I like to draw", "I listen to music"]),
+            //var deltagere = new Deltager[]
+            //{
+            //    new Deltager("Elida", 30, "Blondt",
+            //        ["Jeg liker å klatre rundt", "Jeg er skikkelig dårlig til å synge"]),
+            //    new Deltager("Christian", 36, "Havanna", ["I like to draw", "I listen to music"]),
 
-            };
+            //};
 
-            //deltagere[0].PrintText();
+            List<Deltager> deltagere = new List<Deltager>();
+
+            Deltager Elida = new Deltager("Elida", 30, "Blondt",
+                ["Jeg liker å klatre rundt", "Jeg er skikkelig dårlig til å synge"]);
+
+            Deltager Christian = new Deltager("Christian", 36, "Havanna", ["I like to draw", "I listen to music"]);
+
+            deltagere.Add(Elida);
+            deltagere.Add(Christian);
+
+            Console.WriteLine("Vi er: ");
+            Console.WriteLine();
 
             foreach (var deltager in deltagere)
             {
@@ -22,7 +33,27 @@ namespace ParProgUke1
                 Console.WriteLine();
             }
 
-            
+            Console.WriteLine("Hvem er du? ");
+            Console.Write("Navn: ");
+            string Navn = Console.ReadLine();
+            Console.Write("Alder: ");
+            int Alder = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Hårfarge: ");
+            string Haarfarge = Console.ReadLine();
+            Console.Write("Hobby1: ");
+            string Hobby1 = Console.ReadLine();
+            Console.Write("Hobby2: ");
+            string Hobby2 = Console.ReadLine();
+
+            Deltager nyVenn = new Deltager(Navn, Alder, Haarfarge, [Hobby1, Hobby2]);
+
+            deltagere.Add(nyVenn);
+
+            Console.WriteLine();
+            Thread.Sleep(400);
+            Console.WriteLine("Heisann, du er: ");
+            nyVenn.PrintText();
+
         }
     }
 }
